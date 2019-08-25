96242bd6e985776d45e862c27d8d5b2dd0af47bd
Vitor Hugo Pereira Ribeiro <vitorhugopereiraribeiro@gmail.com>
2019-08-25 16:21:22 -0400

    Ajustando lista paginada

--- Dominio/_Comum/ListaPaginada.cs
+++ Dominio/_Comum/ListaPaginada.cs
@@ -6,7 +6,7 @@ namespace Biblioteca.Dominio._Comum
 {
     public class ListaPaginada<TEntity> where TEntity: class
     {
-        private readonly List<TEntity> _itens;
+        private readonly List<TEntity> _itens = new List<TEntity>();
         public IEnumerable<TEntity> Itens => _itens;
         public int TotalDeItens { get; }
         public int QuantidadeDeItensPorPagina { get; }
@@ -15,17 +15,17 @@ namespace Biblioteca.Dominio._Comum
 
         public ListaPaginada(IEnumerable<TEntity> itens, int quantidadeDeItensPorPagina, int paginaAtual)
         {
-            TotalDeItens = itens.Count();
             PaginaAtual = paginaAtual;
             QuantidadeDeItensPorPagina = quantidadeDeItensPorPagina;
             TotalDePaginas = (int)Math.Ceiling((decimal)(TotalDeItens/QuantidadeDeItensPorPagina));
+            Paginar(itens, quantidadeDeItensPorPagina, paginaAtual);
         }
 
-        private void Paginar(int quantidadeDeItensPorPagina, int paginaAtual)
+        public void Paginar(IEnumerable<TEntity> itens, int quantidadeDeItensPorPagina, int paginaAtual)
         {
             var itensPaginados = paginaAtual == 1
-                ? Itens.Take(quantidadeDeItensPorPagina).ToList()
-                : Itens.Take(paginaAtual * quantidadeDeItensPorPagina).ToList();
+                ? itens.Take(quantidadeDeItensPorPagina).ToList()
+                : itens.Skip((paginaAtual - 1) * quantidadeDeItensPorPagina).Take(quantidadeDeItensPorPagina);
             _itens.AddRange(itensPaginados);
         }
     }
