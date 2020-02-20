666640d3c30d6674cd3e617d58fdee0169c42daf
Vitor Hugo Pereira Ribeiro <vitorhugopereiraribeiro@gmail.com>
2020-02-20 19:50:05 -0400

    Adicionando teste que garanta que a ronda está sendo chamada no acionamento do alarme

--- Dominio.Testes/Teste de Unidade/Aplicacao/CentralDeEmergenciasTeste.cs
+++ Dominio.Testes/Teste de Unidade/Aplicacao/CentralDeEmergenciasTeste.cs
@@ -2,6 +2,7 @@
 using Departamento.De.Policia.Aplicacao._Comum;
 using Departamento.De.Policia.Dominio.DepartamentosDePolicias;
 using Moq;
+using System;
 using Xunit;
 
 namespace Departamento.De.Policia.Testes.Teste_de_Unidade.Aplicacao
@@ -24,6 +25,22 @@ namespace Departamento.De.Policia.Testes.Teste_de_Unidade.Aplicacao
 
             var mensagemObtida = Assert.Throws<ExcecaoDeAplicacao>(Acao).Message;
             Assert.Equal(mensagemEsperada, mensagemObtida);
+        }
+
+        [Fact]
+        public void Deve_fazer_ronda_ao_acionar_o_alarme()
+        {
+            const int numeroDeRegistroDoDepartamento = 1;
+            var departamentoDePolicias = new Mock<DepartamentoDePoliciais>{CallBase = true};
+            var departamentoDePoliciasRepositorio = new Mock<IDepartamentoDePoliciaisRepositorio>();
+            var centralDeEmergencias = new CentralDeEmergencias(departamentoDePoliciasRepositorio.Object);
+            departamentoDePoliciasRepositorio
+                .Setup(dp => dp.ObterDepartamentoDePoliciaPorNumeroDeRegistro(numeroDeRegistroDoDepartamento))
+                .Returns(departamentoDePolicias.Object);
+
+            centralDeEmergencias.AcionarAlarme(numeroDeRegistroDoDepartamento);
+
+            departamentoDePolicias.Verify(dp => dp.DisparouAlarmeHandler(It.IsAny<object>(), It.IsAny<EventArgs>()));
         }
     }
 }
